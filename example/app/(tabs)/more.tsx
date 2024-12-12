6a4f5af4490eeddb6a458df49717a22826e64287
mbilenko <129734190+mbilenko-florio@users.noreply.github.com>
2024-12-12 20:46:59 +0100

    Refine more menu, add chat example

--- example/app/(tabs)/more.tsx
+++ example/app/(tabs)/more.tsx
@@ -1,13 +1,13 @@
 import { ThemedText } from "@/components/ThemedText";
 import { LegendList } from "@legendapp/list";
-import { Link } from "expo-router";
+import { Link, type LinkProps } from "expo-router";
 import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 type ListElement = {
     id: number;
     title: string;
-    url: "/initial-scroll-index";
+    url: LinkProps["href"];
 };
 
 const data: ListElement[] = [
@@ -16,13 +16,21 @@ const data: ListElement[] = [
         title: "Initial scroll index precise navigation",
         url: "/initial-scroll-index",
     },
+    {
+        id: 2,
+        title: "Chat example",
+        url: "/chat-example",
+    },
     // Add more items as needed
 ];
+
+const RightIcon = () => <ThemedText type="subtitle">›</ThemedText>;
 
 const ListItem = ({ title, url }: ListElement) => (
     <Link href={url}>
         <View style={styles.item}>
             <ThemedText>{title}</ThemedText>
+            <RightIcon />
         </View>
     </Link>
 );
@@ -49,6 +57,8 @@ const styles = StyleSheet.create({
         borderBottomColor: "#ccc",
         borderBottomWidth: 1,
         width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
     },
 });
 
