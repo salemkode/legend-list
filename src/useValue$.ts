2bf99f11e68c8843a50ce091098372cee8618e32
Jay Meistrich <jmeistrich@gmail.com>
2024-12-28 10:50:11 +0800

    Polyfill useAnimatedValue because it doesn't work in react-native-web

--- src/useValue$.ts
+++ src/useValue$.ts
@@ -1,7 +1,13 @@
-import { useMemo } from 'react';
-import { useAnimatedValue } from 'react-native';
+import { useMemo, useRef } from 'react';
+import { Animated, useAnimatedValue as _useAnimatedValue } from 'react-native';
 import { listen$, peek$, useStateContext } from './state';
 import type { ListenerType } from './state';
+
+const useAnimatedValue =
+    _useAnimatedValue ||
+    ((initialValue: number): Animated.Value => {
+        return useRef(new Animated.Value(initialValue)).current;
+    });
 
 export function useValue$(key: ListenerType, getValue?: (value: number) => number, key2?: ListenerType) {
     const ctx = useStateContext();
