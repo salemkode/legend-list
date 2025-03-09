ab1e6158ee60fc04e7dffede67ec6d704bdefcc1
Michael Bilenko <129734190+mbilenko-florio@users.noreply.github.com>
2025-03-09 10:23:47 +0100

    fix scrollto

---
+++ example/app/accurate-scrollto/index.tsx
@@ -0,0 +1,117 @@
+import { type Item, renderItem } from "@/app/cards-renderItem";
+import { DRAW_DISTANCE, ESTIMATED_ITEM_LENGTH } from "@/constants/constants";
+import { LegendList, type LegendListRef } from "@legendapp/list";
+import { useRef, useState } from "react";
+import { Button, Platform, StatusBar, StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+interface CardsProps {
+    numColumns?: number;
+}
+
+export default function Cards({ numColumns = 1 }: CardsProps) {
+    const listRef = useRef<LegendListRef>(null);
+
+    const [data, setData] = useState<Item[]>(
+        () =>
+            Array.from({ length: 1000 }, (_, i) => ({
+                id: i.toString(),
+            })) as any[],
+    );
+
+    const buttonText = useRef<string>(null);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.searchContainer}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Select item to scroll to"
+                    clearButtonMode="while-editing"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    onChangeText={(text) => {
+                        buttonText.current = text;
+                    }}
+                />
+                <Button
+                    title="Scroll to item"
+                    onPress={() => {
+                        const index = Number(buttonText.current) || 0;
+                        console.log("scrolling to index", index);
+                        if (index !== -1) {
+                            listRef.current?.scrollToIndex({ index, animated: true });
+                        }
+                    }}
+                />
+            </View>
+            <LegendList
+                ref={listRef}
+                contentContainerStyle={styles.listContainer}
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={(item) => `id${item.id}`}
+                estimatedItemSize={ESTIMATED_ITEM_LENGTH + 120}
+                drawDistance={DRAW_DISTANCE}
+                maintainVisibleContentPosition
+                recycleItems={true}
+                numColumns={numColumns}
+                ListEmptyComponent={
+                    <View style={styles.listEmpty}>
+                        <Text style={{ color: "white" }}>Empty</Text>
+                    </View>
+                }
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    listHeader: {
+        alignSelf: "center",
+        height: 100,
+        width: 100,
+        backgroundColor: "#456AAA",
+        borderRadius: 12,
+        marginHorizontal: 8,
+        marginVertical: 8,
+    },
+    listEmpty: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#6789AB",
+        paddingVertical: 16,
+    },
+    outerContainer: {
+        backgroundColor: "#456",
+        bottom: Platform.OS === "ios" ? 82 : 0,
+    },
+    scrollContainer: {},
+    listContainer: {
+        width: 400,
+        maxWidth: "100%",
+        marginHorizontal: "auto",
+    },
+    searchContainer: {
+        padding: 8,
+        backgroundColor: "#fff",
+        borderBottomWidth: 1,
+        borderBottomColor: "#e0e0e0",
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    searchInput: {
+        height: 40,
+        backgroundColor: "#f5f5f5",
+        borderRadius: 8,
+        paddingHorizontal: 12,
+        fontSize: 16,
+        flexGrow: 1,
+    },
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+        backgroundColor: "#f5f5f5",
+    },
+});
