56390466387b90758874aee876cbaa1022104fd6
Jay Meistrich <jmeistrich@gmail.com>
2025-04-12 22:50:59 +0800

    Fix types in KeyboardAvoidingLegendList

--- src/keyboard-controller.tsx
+++ src/keyboard-controller.tsx
@@ -1,14 +1,23 @@
-import { type ComponentProps, type ForwardedRef, useState } from "react";
+import { LegendList, type LegendListProps, type LegendListRef } from "@legendapp/list";
+import type { AnimatedLegendList } from "@legendapp/list/animated";
+import type { AnimatedLegendList as ReanimatedLegendList } from "@legendapp/list/reanimated";
+// biome-ignore lint/style/useImportType: Leaving this out makes it crash in some environments
+import * as React from "react";
+import { type ForwardedRef, forwardRef, useState } from "react";
 import { useKeyboardHandler } from "react-native-keyboard-controller";
 import { runOnJS } from "react-native-reanimated";
-import { LegendList } from "./LegendList";
-import type { AnimatedLegendList } from "./animated";
-import type { AnimatedLegendList as ReanimatedLegendList } from "./reanimated";
-import { type LegendListRef, typedForwardRef } from "./types";
+
+// biome-ignore lint/complexity/noBannedTypes: This is a workaround for the fact that forwardRef is not typed
+type TypedForwardRef = <T, P = {}>(
+    render: (props: P, ref: React.Ref<T>) => React.ReactNode,
+) => (props: P & React.RefAttributes<T>) => React.ReactNode;
+
+const typedForwardRef = forwardRef as TypedForwardRef;
 
 export const KeyboardAvoidingLegendList = typedForwardRef(function KeyboardAvoidingLegendList<
-    T extends typeof LegendList | typeof AnimatedLegendList | typeof ReanimatedLegendList = typeof LegendList,
->(props: ComponentProps<T> & { LegendList?: T }, forwardedRef: ForwardedRef<LegendListRef>) {
+    ItemT,
+    ListT extends typeof LegendList | typeof AnimatedLegendList | typeof ReanimatedLegendList = typeof LegendList,
+>(props: LegendListProps<ItemT> & { LegendList?: ListT }, forwardedRef: ForwardedRef<LegendListRef>) {
     const { LegendList: LegendListProp, ...rest } = props;
     const [padding, setPadding] = useState(0);
 
